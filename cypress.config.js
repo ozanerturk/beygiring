ed0b8976987467c89bd10ad24134a6dc131a62dc
Ozan ERTÜRK <ozanerturk@sensemore.io>
2025-06-27 14:20:44 +0300

    test config updated

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',  // your dev server URL
     supportFile: 'test/e2e/support.e2e.ts', // adjust if needed
-    specPattern: 'test/e2e/**/*.spec.ts',
+    specPattern: 'test/e2e/**/*.e2e.ts',
   },
 });
